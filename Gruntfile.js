f0795662a258a021d21f888f135eef2c7033a8c3
Naoki Sekiguchi <naokis@ranagram.com>
2014-04-08 21:25:03 +0900

    Grunt setting

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-rename');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      coffee: {
+        compileWithMaps: {
+          options: {
+            sourceMap: true,
+            sourceMapDir: 'maps/'
+          },
+          files: {
+            'js/smoothscroll.js': '_coffee/*.coffee',
+          }
+        }
+      },
+      compass: {
+        options: {
+          httpPath: '/smoothscroll/',
+          sassDir: '_scss',
+          cssDir: 'css',
+          imagesDir: 'img',
+          relativeAssets: true
+        },
+        dev: {
+          options: {
+            environment: 'development',
+            outputStyle: 'compact',
+            noLineComments: true,
+            assetCacheBuster: false
+          }
+        }
+      },
+      coffeelint: {
+        // DOC: https://github.com/vojtajina/grunt-coffeelint
+        // DOC: http://www.coffeelint.org/
+        //            app: [ '_coffee/*.coffee' ],
+        tests: {
+          files: {
+            src: [ '_coffee/*.coffee' ]
+          },
+          options: {
+            'no_trailing_whitespace': {
+              'level': 'error'
+            }
+          }
+        }
+      },
+      copy: {
+        main: {
+          files: [
+            {
+              expand: true,
+              cwd: 'bower_components/jquery/dist/',
+              src: [ 'jquery.min.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/jquery-1.11.0/',
+              src: [ '_temp_jquery.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/underscore/',
+              src: [ 'underscore.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/backbone/',
+              src: [ 'backbone.js' ],
+              dest: 'js/'
+            }
+          ]
+        }
+      },
+      rename: {
+        main: {
+          files: [
+            {
+              src: [ 'js/_temp_jquery.js' ],
+              dest: 'js/jquery-1.11.0.min.js'
+            }
+          ]
+        }
+      },
+      watch: {
+        scss: {
+          files: [ '_scss/*.scss' ],
+          tasks: [ 'compass:dev' ]
+        },
+        coffee: {
+          files: [ '.coffee' ],
+          tasks: [ 'coffeelint' ]
+        }
+      }
+  });
+
+  grunt.registerTask('default', [ 'compass:dev', 'coffee', 'coffeelint', 'watch' ]);
+  grunt.registerTask('deploy', [ 'copy', 'rename', 'compass:dev', 'coffee' ]);
+};
--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,56 +1,6 @@
-/**
- * Smooth Scroll
- *
- * @author     Naoki Sekiguchi
- * @copyright  2012 Naoki Sekiguchi
- * @url        http://likealunatic.jp/
- * @url        http://ranagram.com/
- * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
- * @require    jQuery.js, Underscore.js, Backbone.js
- * @version    Release: 1.0
- * @since      2012-12-20 12:06:50
- */
+(function() {
 
-;(function($, _, window, document, undefined) {
 
-/**
- * smooth scroll
- */
-$.SmoothScroll = Backbone.View.extend({
-	el: 'a[href^="#"]',
-	initialize: function (options) {
-		// options
-		this.opt = {
-			complete: function () { }
-		};
-		_.extend(this.opt, options);
-		this.detectScrollBody();
-	},
-	events: {
-		'click': 'scroll'
-	},
-	detectScrollBody: function () {
-		var $win = $(window);
-		var currentScrollTop = $win.scrollTop();
-		$win.scrollTop(currentScrollTop + 1);
-		if ($('html').scrollTop() > 0) {
-			this.$body = $('html');
-		} else if ($('body').scrollTop() > 0) {
-			this.$body = $('body');
-		}
-		$win.scrollTop(currentScrollTop);
-	},
-	scroll: function (e) {
-		var target = e.currentTarget || e.originalEvent.currentTarget;
-		var href = $(target).attr('href');
-		if (typeof href != 'string') { return true; }
-		href = href.replace(/(#[a-zA-Z0-9_\-]+)$/, '$1');
-		if (!$(href)[0]) { return true; }
-		this.$body.stop(true).animate({
-			scrollTop: $(href).offset().top
-		}, 750, 'swing', this.opt.complete);
-		e.preventDefault();
-	}
-});
+}).call(this);
 
-})(jQuery, _, this, this.document);
+//# sourceMappingURL=../maps/smoothscroll.js.map
