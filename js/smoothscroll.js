6faa82f66aca6650d77cf4968cd2a0e25190f8df
Naoki Sekiguchi <naokis@ranagram.com>
2014-04-09 18:30:45 +0900

    Migrated to CoffeeScript\!

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,61 @@
-(function() {
 
+/*
+Smooth Scroll
+
+@author     Naoki Sekiguchi
+@copyright  2014 Naoki Sekiguchi
+@url        http://likealunatic.jp/
+@license    http://www.opensource.org/licenses/mit-license.html  MIT License
+@require    jQuery.js, Underscore.js, Backbone.js
+@version    Release: 1.1
+@since      2012-12-20 12:06:50
+ */
+
+(function() {
+  $.SmoothScroll = Backbone.View.extend({
+    el: 'a[href^="#"]',
+    initialize: function(options) {
+      this.opt = {
+        complete: function() {
+          return null;
+        }
+      };
+      _.extend(this.opt, options);
+      this.detectScrollBody();
+      return this;
+    },
+    events: {
+      'click': 'scroll'
+    },
+    detectScrollBody: function() {
+      var $win, currentScrollTop;
+      $win = $(window);
+      currentScrollTop = $win.scrollTop();
+      $win.scrollTop(currentScrollTop + 1);
+      if ($('html').scrollTop() > 0) {
+        this.$body = $('html');
+      } else if ($('body').scrollTop() > 0) {
+        this.$body = $('body');
+      }
+      $win.scrollTop(currentScrollTop);
+      return this;
+    },
+    scroll: function(e) {
+      var href, target;
+      target = e.currentTarget || e.originalEvent.currentTarget;
+      href = $(target).attr('href');
+      if (typeof href !== 'string') {
+        return true;
+      }
+      href = href.replace(/(#[a-zA-Z0-9_\-]+)$/, '$1');
+      if ($(href)[0] != null) {
+        this.$body.stop(true).animate({
+          scrollTop: $(href).offset().top
+        }, 750, 'swing', this.opt.complete);
+      }
+      e.preventDefault();
+      return this;
+    }
+  });
 
 }).call(this);
-
-//# sourceMappingURL=../maps/smoothscroll.js.map
